8ce555b99aa0b575c10dc54228f9efd821ed448c
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-15 10:32:36 +0100

    Adding write sample for the YAML provider

--- docs/content/YamlProvider.fsx
+++ docs/content/YamlProvider.fsx
@@ -11,12 +11,10 @@ It's generated, hence the types can be used from any .NET language, not only fro
 
 It can produce mutable properties for Scalars (leafs), which means the object tree can be loaded, modified and saved into the original file or a stream as Yaml text. Adding new properties is not supported, however lists can be replaced with new ones atomically. This is intentionally, see below.
 
-The main initial purpose for this is to be used as part of a statically typed application configuration system which would have a single master source of configuration structure - a Yaml file. Than any F#/C# project in a solution will able to use the generated read-only object graph.
+The main purpose for this is to be used as part of a statically typed application configuration system which would have a single master source of configuration structure - a Yaml file. Then any F#/C# project in a solution will able to use the generated read-only object graph.
 
 When you push a system into production, you can modify the configs with scripts written in F# in safe, statically checked way with full intellisense.
 
-Examples
-========
 
 Using configuration from F# scripts
 -----------------------------------
@@ -50,8 +48,17 @@ open FSharp.Configuration
 
 // Let the type provider do it's work
 type Settings = Yaml<"Settings.yaml">
+let settings = Settings()
 
-let db = Settings.DB_Type()
-db.ConnectionString
+// read a value from the config
+settings.DB.ConnectionString
 // [fsi:val it : string = ]
 // [fsi:  "Data Source=server1;Initial Catalog=Database1;Integrated Security=SSPI;"]
+
+// change a value in the config
+settings.DB.ConnectionString <- "Data Source=server2;"
+settings.DB.ConnectionString
+// [fsi:val it : string = "Data Source=server2;"]
+
+// write the settings back to a yaml file
+settings.Save(__SOURCE_DIRECTORY__ + @"\ChangedSettings.yaml")
--- tests/FSharp.Configuration.Tests/YamlProvider.Tests.fs
+++ tests/FSharp.Configuration.Tests/YamlProvider.Tests.fs
@@ -5,28 +5,25 @@ open NUnit.Framework
 open FsUnit
 
 type Settings = Yaml<"Settings.yaml">
-let db = Settings.DB_Type()
+let settings = Settings()
 
 [<Test>] 
 let ``Can return a string from the settings file``() =   
-    db.ConnectionString.GetType() |> should equal typeof<string>   
-    db.ConnectionString |> should equal "Data Source=server1;Initial Catalog=Database1;Integrated Security=SSPI;"
+    settings.DB.ConnectionString.GetType() |> should equal typeof<string>   
+    settings.DB.ConnectionString |> should equal "Data Source=server1;Initial Catalog=Database1;Integrated Security=SSPI;"
 
 [<Test>] 
 let ``Can return an int from the settings file``() =   
-    db.NumberOfDeadlockRepeats.GetType() |> should equal typeof<int>   
-    db.NumberOfDeadlockRepeats |> should equal 5
+    settings.DB.NumberOfDeadlockRepeats.GetType() |> should equal typeof<int>   
+    settings.DB.NumberOfDeadlockRepeats |> should equal 5
 
 [<Test>] 
 let ``Can return a TimeSpan from the settings file``() =   
-    db.DefaultTimeout.GetType() |> should equal typeof<System.TimeSpan>   
-    db.DefaultTimeout |> should equal (System.TimeSpan.FromMinutes 5.)
-
-
-let mail = Settings.Mail_Type()
+    settings.DB.DefaultTimeout.GetType() |> should equal typeof<System.TimeSpan>   
+    settings.DB.DefaultTimeout |> should equal (System.TimeSpan.FromMinutes 5.)
 
 [<Test>] 
 let ``Can return a list from the settings file``() =       
-    mail.ErrorNotificationRecipients.Count |> should equal 2
-    mail.ErrorNotificationRecipients.[0] |> should equal "user1@sample.com"
-    mail.ErrorNotificationRecipients.[1] |> should equal "user2@sample.com"
+    settings.Mail.ErrorNotificationRecipients.Count |> should equal 2
+    settings.Mail.ErrorNotificationRecipients.[0] |> should equal "user1@sample.com"
+    settings.Mail.ErrorNotificationRecipients.[1] |> should equal "user2@sample.com"
