32cb9884180aa8f28d18f438e58959b4ab1e609f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-16 16:26:53 +0100

    Refactor ResXProvider

--- src/FSharp.Configuration/ResXProvider.fs
+++ src/FSharp.Configuration/ResXProvider.fs
@@ -6,12 +6,30 @@ open System.Reflection
 open Microsoft.FSharp.Core.CompilerServices
 open Samples.FSharp.ProvidedTypes
 open FSharp.Configuration.Helper
+open System.Resources
+open System.ComponentModel.Design
+open System.Collections
+
+/// Converts ResX entries to provided properties
+let toProperties (resXFilePath:string) =       
+    use reader = new ResXResourceReader(resXFilePath)
+    reader.UseResXDataNodes <- true
+    [|for (entry:DictionaryEntry) in reader |> Seq.cast ->                   
+        let node = entry.Value :?> ResXDataNode
+        let name = string node.Name
+        let value = node.GetValue(Unchecked.defaultof<ITypeResolutionService>)
+        let comment = node.Comment
+        let getter args = <@@ value @@>
+        let resource = ProvidedProperty(name, typeof<string>, IsStatic=true, GetterCode=getter)                          
+        if not(String.IsNullOrEmpty(comment)) then resource.AddXmlDoc(node.Comment)
+        resource
+    |]
 
 /// Creates resource data type from specified ResX file
 let createResourceDataType (resXFilePath) =
-    let resourceName = System.IO.Path.GetFileNameWithoutExtension(resXFilePath)
+    let resourceName = Path.GetFileNameWithoutExtension(resXFilePath)
     let data = ProvidedTypeDefinition(resourceName, baseType=Some typeof<obj>, HideObjectMethods=true)
-    let properties = ResX.toProperties resXFilePath
+    let properties = toProperties resXFilePath
     for p in properties do data.AddMember(p)
     data
 
