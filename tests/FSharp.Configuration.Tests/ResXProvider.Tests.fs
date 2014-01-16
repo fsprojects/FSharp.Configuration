2b8cb29b03576ff5331856d0ceaa8897bd14e791
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-01-16 16:24:26 +0100

    Adding image to Resource1.resx

--- tests/FSharp.Configuration.Tests/ResXProvider.Tests.fs
+++ tests/FSharp.Configuration.Tests/ResXProvider.Tests.fs
@@ -8,5 +8,5 @@ type ResX = ResXProvider<file="Resource1.resx">
 
 [<Test>] 
 let ``Can return a string from the resource file``() =   
-    ResX.Resource1.HelloWorld.GetType() |> should equal typeof<string>   
-    ResX.Resource1.HelloWorld |> should equal "Hello World"
+    ResX.Resource1.Greetings.GetType() |> should equal typeof<string>   
+    ResX.Resource1.Greetings |> should equal "Hello World!"
