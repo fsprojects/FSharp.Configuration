40ee0719a6f96e05b0839b482aedaa898e8a86df
Steven Taylor <stevesay7@gmail.com>
2014-01-17 07:25:45 +0000

    FSI / .fsx failure + script to show this
    scripts + FSI session info.  Can this bug can be seen elsewhere?

---
+++ tests/FSharp.Configuration.Tests/AppSettingsProvider.fsx
@@ -0,0 +1,14 @@
+﻿#r"System"
+#r"System.Management"
+#r"System.Configuration"
+#r"bin/debug/FSharp.Configuration.dll"
+
+open System
+open System.Configuration
+open FSharp.Configuration
+
+type settings = AppSettings<"app.config">
+settings.Test2
+settings.TestBool
+settings.Test2
+
