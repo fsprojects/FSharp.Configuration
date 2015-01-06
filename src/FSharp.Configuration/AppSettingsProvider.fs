d32da53ff4f8fec1988068883dea2bebbacef9c1
vasily-kirichenko <vaskir@gmail.com>
2015-01-06 21:17:14 +0300

    fixed: AppSettingsProvider does not read connection strings from web.config in runtime

--- src/FSharp.Configuration/AppSettingsProvider.fs
+++ src/FSharp.Configuration/AppSettingsProvider.fs
@@ -12,27 +12,25 @@ open System.Web.Hosting
 
 let private getConfig() =
     if HostingEnvironment.IsHosted then
-        Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~")
-    else
-        ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
+        Web.Configuration.WebConfigurationManager.OpenWebConfiguration "~"
+    else ConfigurationManager.OpenExeConfiguration ConfigurationUserLevel.None
 
-let getConfigValue(key) =
+let getConfigValue key =
     let settings = getConfig().AppSettings.Settings
     settings.[key].Value
 
-let setConfigValue(key, value) = 
+let setConfigValue (key, value) = 
     let config = getConfig() 
     config.AppSettings.Settings.[key].Value <- value
     config.Save()
 
-let getConnectionString(key: string) =
-    let connectionStrings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None).ConnectionStrings.ConnectionStrings
-    connectionStrings.[key].ConnectionString
+let getConnectionString (key: string) =
+    getConfig().ConnectionStrings.ConnectionStrings.[key].ConnectionString
 
-let setConnectionString(key:string, value) =
-    let settings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None)
-    settings.ConnectionStrings.ConnectionStrings.[key].ConnectionString <- value
-    settings.Save()
+let setConnectionString (key: string, value) =
+    let config = getConfig()
+    config.ConnectionStrings.ConnectionStrings.[key].ConnectionString <- value
+    config.Save()
 
 let internal typedAppSettings (context: Context) =
     let appSettings = erasedType<obj> thisAssembly rootNamespace "AppSettings"
@@ -79,27 +77,27 @@ let internal typedAppSettings (context: Context) =
                             | ValueParser.Uri _ ->
                                 ProvidedProperty(name, typeof<Uri>,
                                     GetterCode = (fun _ -> <@@ Uri (getConfigValue key) @@>),
-                                    SetterCode = fun args -> <@@ setConfigValue(key, string ((%%args.[0]):Uri)) @@>)
+                                    SetterCode = fun args -> <@@ setConfigValue(key, string (%%args.[0]: Uri)) @@>)
                             | ValueParser.Int _ ->
                                 ProvidedProperty(name, typeof<int>,
                                     GetterCode = (fun _ -> <@@ Int32.Parse (getConfigValue key) @@>),
-                                    SetterCode = fun args -> <@@ setConfigValue(key, string ((%%args.[0]):Int32)) @@>)
+                                    SetterCode = fun args -> <@@ setConfigValue(key, string (%%args.[0]: Int32)) @@>)
                             | ValueParser.Bool _ ->
                                 ProvidedProperty(name, typeof<bool>,
                                     GetterCode = (fun _ -> <@@ Boolean.Parse (getConfigValue key) @@>),
-                                    SetterCode = fun args -> <@@ setConfigValue(key, string ((%%args.[0]):Boolean)) @@>)
+                                    SetterCode = fun args -> <@@ setConfigValue(key, string (%%args.[0]: Boolean)) @@>)
                             | ValueParser.Float _ ->
                                 ProvidedProperty(name, typeof<float>,
                                     GetterCode = (fun _ -> <@@ Double.Parse (getConfigValue key, NumberStyles.Any, CultureInfo.InvariantCulture) @@>),
-                                    SetterCode = fun args -> <@@ setConfigValue(key, string ((%%args.[0]):float)) @@>)
+                                    SetterCode = fun args -> <@@ setConfigValue(key, string (%%args.[0]: float)) @@>)
                             | ValueParser.TimeSpan _ ->
                                 ProvidedProperty(name, typeof<TimeSpan>,
                                     GetterCode = (fun _ -> <@@ TimeSpan.Parse(getConfigValue key, CultureInfo.InvariantCulture) @@>),
-                                    SetterCode = fun args -> <@@ setConfigValue(key, string ((%%args.[0]):TimeSpan)) @@>)
+                                    SetterCode = fun args -> <@@ setConfigValue(key, string (%%args.[0]: TimeSpan)) @@>)
                             | ValueParser.DateTime _ ->
                                 ProvidedProperty(name, typeof<DateTime>,
                                     GetterCode = (fun _ -> <@@ DateTime.Parse(getConfigValue key, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal) @@>),
-                                    SetterCode = fun args -> <@@ setConfigValue(key, ((%%args.[0]):DateTime).ToString("o")) @@>)
+                                    SetterCode = fun args -> <@@ setConfigValue(key, (%%args.[0]: DateTime).ToString("o")) @@>)
                             | _ ->
                                 ProvidedProperty(name, typeof<string>,
                                     GetterCode = (fun _ -> <@@ getConfigValue key @@>),
@@ -107,20 +105,18 @@ let internal typedAppSettings (context: Context) =
 
                         prop.IsStatic <- true
                         prop.AddXmlDoc (sprintf "Returns the value from %s with key %s" configFileName key)
-                        prop.AddDefinitionLocation(1,1,filePath)
+                        prop.AddDefinitionLocation(1, 1, filePath)
 
                         typeDef.AddMember prop
 
-                    let name = niceName names "ConfigFileName"
-                    let getValue = <@@ filePath @@>
-                    let prop = ProvidedProperty(name, typeof<string>, GetterCode = (fun _ -> getValue))
+                    let prop = 
+                        ProvidedProperty(niceName names "ConfigFileName", typeof<string>, GetterCode = fun _ -> <@@ filePath @@>)
 
                     prop.IsStatic <- true
                     prop.AddXmlDoc "Returns the Filename"
-
                     typeDef.AddMember prop
 
-                    let connectionStringTypeDefinition = typedConnectionStrings(config,filePath,configFileName)
+                    let connectionStringTypeDefinition = typedConnectionStrings (config, filePath, configFileName)
                     typeDef.AddMember connectionStringTypeDefinition
 
                     context.WatchFile filePath
