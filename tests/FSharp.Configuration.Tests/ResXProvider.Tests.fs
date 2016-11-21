ae77d2d46d222fc59acaec006c5ab279ca0f2402
efoerster <efoerster@users.noreply.github.com>
2016-11-21 11:10:49 +0100

    Fix ResX provider

--- tests/FSharp.Configuration.Tests/ResXProvider.Tests.fs
+++ tests/FSharp.Configuration.Tests/ResXProvider.Tests.fs
@@ -3,17 +3,21 @@ module FSharp.Configuration.Tests.ResXTests
 open FSharp.Configuration
 open Xunit
 
-type ResX = ResXProvider<file="Resource1.resx">
+type Resource1 = ResXProvider<"Resource1.resx">
 
 [<Fact>] 
-let ``Can return a string from the resource file``() =   
-    Assert.Equal<string>(ResX.Resource1.Greetings, "Hello World!")
+let ``Can return a string from the resource file``() =
+    Assert.Equal<string>(Resource1.Greetings, "Hello World!")
 
 [<Fact>] 
 let ``Can return an image from the resource file``() =
-    Assert.IsType<System.Drawing.Bitmap>(ResX.Resource1.Flowers) |> ignore
-    Assert.NotNull ResX.Resource1.Flowers
+    Assert.IsType<System.Drawing.Bitmap>(Resource1.Flowers) |> ignore
+    Assert.NotNull Resource1.Flowers
 
 [<Fact>] 
 let ``Can return an int from the resource file``() =
-    Assert.Equal(ResX.Resource1.Answer, 42)
+    Assert.Equal(Resource1.Answer, 42)
+
+[<Fact>] 
+let ``Can return a text file from the resource file``() =
+    Assert.Equal<string>(Resource1.TextFile, "Text")
