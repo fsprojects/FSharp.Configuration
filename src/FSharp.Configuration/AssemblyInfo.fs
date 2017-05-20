8cfd8656ef70661f58b4de42289f6df6e0704df3
Vasily Kirichenko <vaskir@gmail.com>
2017-05-20 11:19:18 +0300

    Bump version to 1.2.0

--- src/FSharp.Configuration/AssemblyInfo.fs
+++ src/FSharp.Configuration/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Configuration")>]
 [<assembly: AssemblyProductAttribute("FSharp.Configuration")>]
 [<assembly: AssemblyDescriptionAttribute("The FSharp.Configuration project contains type providers for the configuration of .NET projects.")>]
-[<assembly: AssemblyVersionAttribute("1.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.0")>]
+[<assembly: AssemblyVersionAttribute("1.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Configuration"
     let [<Literal>] AssemblyProduct = "FSharp.Configuration"
     let [<Literal>] AssemblyDescription = "The FSharp.Configuration project contains type providers for the configuration of .NET projects."
-    let [<Literal>] AssemblyVersion = "1.1.0"
-    let [<Literal>] AssemblyFileVersion = "1.1.0"
+    let [<Literal>] AssemblyVersion = "1.2.0"
+    let [<Literal>] AssemblyFileVersion = "1.2.0"
