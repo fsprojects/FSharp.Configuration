e3c5c47a620b92be6b379614dfee2803d054ed5d
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2017-10-12 14:32:26 +0300

    add ref

--- src/FSharp.Configuration/ConfigTypeProvider.fs
+++ src/FSharp.Configuration/ConfigTypeProvider.fs
@@ -1,22 +1,22 @@
 ﻿module FSharp.Configuration.ConfigTypeProvider
-//
-//open FSharp.Configuration.Helper
-//open Microsoft.FSharp.Core.CompilerServices
-//open ProviderImplementation.ProvidedTypes
-//
-//[<TypeProvider>]
-//type FSharpConfigurationProvider(cfg: TypeProviderConfig) as this =
-//    class
-//        inherit TypeProviderForNamespaces()
-//        let context = new Context(this, cfg)
-//        do this.AddNamespace (
-//            rootNamespace,
-//            [ AppSettingsTypeProvider.typedAppSettings context
-//              ResXProvider.typedResources context
-//              YamlConfigTypeProvider.typedYamlConfig context
-//              IniFileProvider.typedIniFile context ])
-//        do this.Disposing.Add (fun _ -> dispose context)
-//    end
-//
-//[<TypeProviderAssembly>]
-//do ()
+
+open FSharp.Configuration.Helper
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+
+[<TypeProvider>]
+type FSharpConfigurationProvider(cfg: TypeProviderConfig) as this =
+    class
+        inherit TypeProviderForNamespaces()
+        let context = new Context(this, cfg)
+        do this.AddNamespace (
+            rootNamespace,
+            [ //AppSettingsTypeProvider.typedAppSettings context
+              //ResXProvider.typedResources context
+              //YamlConfigTypeProvider.typedYamlConfig context
+              IniFileProvider.typedIniFile context ])
+        do this.Disposing.Add (fun _ -> dispose context)
+    end
+
+[<TypeProviderAssembly>]
+do ()
