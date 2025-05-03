9079d8a5e5398b7ce770e8de1d345fc1e0e953a1
Tuomas Hietanen <thorium@iki.fi>
2025-05-03 12:46:11 +0100

    Updates to F# 8 and .NET 8.0 (#180)
    * Updates to F# 8 and .NET 8.0
    
    * Let's drop System.Configuration.ConfigurationManager to v8 if that would be more suitable for CI builds

--- src/FSharp.Configuration.Runtime/FSharp.Configuration.Runtime.fs
+++ src/FSharp.Configuration.Runtime/FSharp.Configuration.Runtime.fs
@@ -1,5 +1,7 @@
 namespace FSharp.Configuration.Yaml
 
 // Put the TypeProviderAssemblyAttribute in the runtime DLL, pointing to the design-time DLL
+#if TP_RUNTIME
 [<assembly: CompilerServices.TypeProviderAssembly("FSharp.Configuration.DesignTime.dll")>]
 do ()
+#endif
